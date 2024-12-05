81dadcd065196801ea9b579741b2a9636002d7a5
Joe Clark <weirdlookingjay@live.com>
2024-12-05 15:00:35 -0500

    Added Sessions API

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,6 +9,8 @@ import { HTTPSTATUS } from "./config/http.config";
 import { asyncHandler } from "./middlewares/asyncHandler";
 import authRoutes from "./modules/auth/auth.routers";
 import passport from "./middlewares/passport";
+import { authenticateJWT } from "./common/strategies/jwt.strategy";
+import sessionRoutes from "./modules/session/session.route";
 
 const app = express();
 const BASE_PATH = config.BASE_PATH;
@@ -35,6 +37,8 @@ app.get(
 );
 
 app.use(`${BASE_PATH}/auth`, authRoutes);
+
+app.use(`${BASE_PATH}/session`, authenticateJWT, sessionRoutes);
 
 app.use(errorHandler);
 
